28a07d808713ec3ddcd774f941b3f7a0a87b23c6
Nicolas Onate <nionate.on@gmail.com>
2023-04-29 19:15:01 -0400

    adds action button for cart

--- src/components/Cart/ShoppingCartAlert.tsx
+++ src/components/Cart/ShoppingCartAlert.tsx
@@ -2,31 +2,43 @@ import React from "react";
 import { FaShoppingCart } from "react-icons/fa";
 
 import { useCart } from "react-use-cart";
+import useScreenSize from "../../hooks/useScreenSize";
 import { formatCurrencyToCLP } from "../Resources/ResourceDetail/utils";
 
-function ShoppingCartAlert() {
+interface ShoppingCartAlertProps {
+  onClick?: (event: Event) => void;
+}
+
+const ShoppingCartAlert: React.FC<ShoppingCartAlertProps> = (props) => {
   const { totalUniqueItems, cartTotal } = useCart();
+  const { onClick } = props;
+  const isDesktop = useScreenSize();
 
   return (
-    <div className=" flex flex-row justify-end">
+    <div className="md:flex md:flex-row md:justify-end">
       <div className="self-center hover:cursor-pointer">
-        <FaShoppingCart className="self-center text-3xl absolute text-white hover:text-secondary" />
+        <FaShoppingCart
+          onClick={onClick as any}
+          className="self-center bottom-4 left-4 md:bottom-auto md:left-auto text-xl md:text-3xl absolute text-white md:hover:text-secondary"
+        />
         {totalUniqueItems > 0 ? (
-          <span className="bg-danger text-white text-sm font-bold pl-1 pr-1 rounded-full relative left-7  bottom-3">
+          <span className="bg-danger text-white text-sm font-bold pl-1 pr-1 rounded-full relative left-4 md:left-7 bottom-3">
             {totalUniqueItems}
           </span>
         ) : (
-          <span className="bg-gray-600 text-white text-sm font-bold pl-1 pr-1 rounded-full relative left-7  bottom-3">
+          <span className="bg-gray-600 text-white text-sm font-bold pl-1 pr-1 rounded-full relative left-4 md:left-7 bottom-3">
             0
           </span>
         )}
       </div>
-      <div className="pl-10 mr-10 self-center">
-        <span className="font-bold">Total</span> :{" "}
-        {formatCurrencyToCLP(cartTotal)}
-      </div>
+      {isDesktop && (
+        <div className="pl-10 mr-10 self-center">
+          <span className="font-bold">Total</span> :{" "}
+          {formatCurrencyToCLP(cartTotal)}
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default ShoppingCartAlert;
