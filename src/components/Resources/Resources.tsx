2c6a8ea0f44827d00a4e3a6dd84eb8743399544f
Nicolas Onate <nionate.on@gmail.com>
2023-05-03 22:57:58 -0400

    adds pagination component

--- src/components/Resources/Resources.tsx
+++ src/components/Resources/Resources.tsx
@@ -1,25 +1,24 @@
 import { useQuery } from "@tanstack/react-query";
-import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-import { SearchResults } from "../Search/SearchResultPanel/interfaces";
+import { useState } from "react";
+//import { useLocation } from "react-router-dom";
+//import { SearchResults } from "../Search/SearchResultPanel/interfaces";
 import SkeletonWrapper from "../SkeletonWrapper";
 import ResourceTable from "../Videos/ResourceTable";
 import ResourcesSkeleton from "./ResourcesSkeleton";
 import { getAllResources } from "./service";
 
 function Resources() {
-  const location = useLocation();
+  // const location = useLocation();
   const [resourceType, setResourceType] = useState("video");
-  let results =
-    location.state !== null ? (location.state as SearchResults) : undefined;
-  const [dataList, setDataList] = useState<any>([]);
+  /*   let results = location.state !== null ? (location.state as SearchResults) : undefined;
+  const [dataList, setDataList] = useState<any>([]); */
 
   const { data, isLoading } = useQuery(
     ["GET_ALL_RESOURCES", resourceType],
     () => getAllResources(resourceType)
   );
 
-  useEffect(() => {
+  /*   useEffect(() => {
     if (results) {
       setDataList(
         results.results[0].resourceList.concat(results.results[1].resourceList)
@@ -28,14 +27,16 @@ function Resources() {
       console.log("No resources", data);
       setDataList(data?.items);
     }
-  }, [results, data]);
+  }, [results, data]); */
+
+  console.log(data);
 
   return (
     <div className="w-full py-10 px-2 md:px-10">
       <SkeletonWrapper isLoading={isLoading} skeleton={<ResourcesSkeleton />}>
         <ResourceTable
           parent="resources"
-          resourceList={dataList}
+          resourceList={data?.items}
           setResourceType={setResourceType}
           resourceType={resourceType}
         />
